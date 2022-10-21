de5cb4cd1e70bce96554a684f0dfa93bc32a9273
Brandon Herman <brandon.herman@outlook.com>
2022-10-20 22:30:15 -0400

    Fix Styling

--- src/script.js
+++ src/script.js
@@ -185,7 +185,7 @@ window.addEventListener("resize", () => {
 
   // Update renderer
   renderer.setSize(sizes.width, sizes.height);
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  // renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 
 /**
